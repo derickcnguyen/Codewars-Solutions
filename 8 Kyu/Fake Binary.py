e5a76e5c802f3622e8412115d8c40bc15c4625c5
Derick Nguyen <155448611+derickcnguyen@users.noreply.github.com>
2024-01-03 12:57:46 -0500

    Solution to Fake Binary.py

---
+++ 8 Kyu/Fake Binary.py
@@ -0,0 +1,13 @@
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string.
+
+def fake_bin(x):
+    fakeBin = ''
+    convert = list(x)
+    for i in convert:
+        if int(i) >= 5:
+            fakeBin += '1'
+        if int(i) < 5:
+            fakeBin += '0'
+    return fakeBin
