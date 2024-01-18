842b545a5a56681ef023ce1e53df3ba14b1655b6
Derick Nguyen <155448611+derickcnguyen@users.noreply.github.com>
2024-01-18 15:01:07 -0500

    Update Equal Sides Of An Array.py

--- 6 Kyu/Equal Sides Of An Array.py
+++ 6 Kyu/Equal Sides Of An Array.py
@@ -34,3 +34,11 @@ def find_even_index(arr):
         if leftSum == rightSum:
             return i
     return -1
+
+or
+
+def find_even_index(arr):
+    for i in range(len(arr)):
+        if sum(arr[:i]) == sum(arr[i+1:]):
+            return i
+    return -1
