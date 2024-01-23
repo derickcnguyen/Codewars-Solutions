51df6594efb709237d5a80fda770c735c16df471
Derick Nguyen <155448611+derickcnguyen@users.noreply.github.com>
2024-01-23 09:40:32 -0500

    Solution to Find the stray number.py

---
+++ 7 Kyu/Find the stray number.py
@@ -0,0 +1,16 @@
+You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+
+def stray(arr):
+    easy = sorted(arr)
+    if easy[0] == easy[1]:
+        return easy[-1]
+    else:
+        return easy[0]
