c4881da4478abd3c1abe72119167789a52726c29
Derick Nguyen <155448611+derickcnguyen@users.noreply.github.com>
2024-01-26 17:02:59 -0500

    Create  Correct the mistakes of the character recognition software.py

---
+++ 8 Kyu/Correct the mistakes of the character recognition software.py
@@ -0,0 +1,23 @@
+Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+The test cases contain numbers only by mistake.
+
+def correct(s):
+    fixed = ''
+    for letter in s:
+        if letter == '5':
+            fixed += 'S'
+        elif letter == '0':
+            fixed += 'O'
+        elif letter == '1':
+            fixed += 'I'
+        else:
+            fixed += letter
+    return fixed
