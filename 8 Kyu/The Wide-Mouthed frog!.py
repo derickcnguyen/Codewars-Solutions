54c6bf17e04c0b1c3d33e1d545ff43bdc689a108
Derick Nguyen <155448611+derickcnguyen@users.noreply.github.com>
2024-02-23 13:03:05 -0500

    Update The Wide-Mouthed frog!.py

--- 8 Kyu/The Wide-Mouthed frog!.py
+++ 8 Kyu/The Wide-Mouthed frog!.py
@@ -7,4 +7,4 @@ When he meets the alligator, it then makes a tiny mouth.
 Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.
 
 def mouth_size(animal): 
-    return 'small' if animal == 'alligator' else 'wide'
+    return 'small' if animal.lower() == 'alligator' else 'wide'
