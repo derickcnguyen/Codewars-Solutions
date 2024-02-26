2fe6009b294aa7541963a1b5fc1be523a6945640
Derick Nguyen <155448611+derickcnguyen@users.noreply.github.com>
2024-02-26 06:13:33 -0500

    Create Sum of the first nth term of Series.py

---
+++ 7 Kyu/Sum of the first nth term of Series.py
@@ -0,0 +1,21 @@
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+def series_sum(n):
+    if n == 0:
+        return '0.00'
+    series = sum(1/(3*i+1) for i in range(n))
+    result = '{:.2f}'.format(series)
+    return result
