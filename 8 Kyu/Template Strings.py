ca01350864afcc0e784360c67122e5d47d2e01cc
Derick Nguyen <155448611+derickcnguyen@users.noreply.github.com>
2024-05-17 10:00:15 -0400

    Create Template Strings.py

---
+++ 8 Kyu/Template Strings.py
@@ -0,0 +1,8 @@
+Your task is to return the correct string using the Template String Feature.
+Input
+Two Strings, no validation is needed.
+Output
+You must output a string containing the two strings with the word ```' are '```
+
+def temple_strings(obj, feature): 
+    return obj + ' are ' + feature
