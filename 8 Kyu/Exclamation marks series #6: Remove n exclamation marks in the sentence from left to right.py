51240b0eb1a8d2a656f395f769f43c75f23cfef7
Derick Nguyen <155448611+derickcnguyen@users.noreply.github.com>
2024-05-17 14:08:00 -0400

    Create Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right.py

---
+++ 8 Kyu/Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right.py
@@ -0,0 +1,25 @@
+Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+Examples
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+
+def remove(s, n):
+    new_str = ''
+    removed = 0
+    
+    for i in s:
+        if i == '!' and removed < n:
+            removed += 1
+        else:
+            new_str += i 
+    return new_str
