33bfdd2eed3ef925afb84ceccf75d90cdc0726a9
Derick Nguyen <155448611+derickcnguyen@users.noreply.github.com>
2024-07-08 17:10:56 -0400

    Create Calculate Price Excluding VAT.py

---
+++ 8 Kyu/Calculate Price Excluding VAT.py
@@ -0,0 +1,15 @@
+If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
+
+Thus, if your function receives 230.00 as input, it should return 200.00
+
+Notes:
+
+VAT is always 15% for the purposes of this Kata.
+Round the result to 2 decimal places.
+If null value given then return -1
+
+def excluding_vat_price(price):
+    if not price:
+        return -1
+    else:
+        return price * (100/115)
