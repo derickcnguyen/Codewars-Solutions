476f1d5fbe3a89aa8424040939f7233ff5b6c97c
Derick Nguyen <155448611+derickcnguyen@users.noreply.github.com>
2024-12-05 04:50:09 -0500

    Create Digits explosion.py

---
+++ 7 Kyu/Digits explosion.py
@@ -0,0 +1,12 @@
+Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+Examples
+"312" should return "333122"
+"102269" should return "12222666666999999999"
+
+def explode(s):
+    explosion = ''
+    split = list(s)
+    for i in split:
+        explosion += i * int(i)
+    return explosion
