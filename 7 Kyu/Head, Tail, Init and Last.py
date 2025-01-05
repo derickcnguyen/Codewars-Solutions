cbfa455e7c7d1def2b5905ec4c5b30abb69cc9f5
Derick Nguyen <155448611+derickcnguyen@users.noreply.github.com>
2025-01-05 12:27:36 -0500

    Create Head, Tail, Init and Last.py

---
+++ 7 Kyu/Head, Tail, Init and Last.py
@@ -0,0 +1,32 @@
+$ ghci
+GHCi, version 7.6.3: http://www.haskell.org/ghc/  :? for help
+λ head [1,2,3,4,5]
+1
+λ tail [1,2,3,4,5]
+[2,3,4,5]
+λ init [1,2,3,4,5]
+[1,2,3,4]
+λ last [1,2,3,4,5]
+5
+Your job is to implement these functions in your given language. Make sure it doesn't edit the array; that would cause problems! Here's a cheat sheet:
+
+| HEAD | <----------- TAIL ------------> |
+[  1,  2,  3,  4,  5,  6,  7,  8,  9,  10]
+| <----------- INIT ------------> | LAST |
+
+head [x] = x
+tail [x] = []
+init [x] = []
+last [x] = x
+
+def head(lst):
+    return lst[0]
+
+def tail(lst):
+    return lst[1:]
+
+def init(lst):
+    return lst[:-1]
+
+def last(lst):
+    return lst[-1]
