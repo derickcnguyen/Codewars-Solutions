7ad7c8c17fbc74455c0a021efe6713d1b144da52
Derick Nguyen <155448611+derickcnguyen@users.noreply.github.com>
2025-01-12 10:28:23 -0500

    Create Number Of Occurrences.py

---
+++ 7 Kyu/Number Of Occurrences.py
@@ -0,0 +1,15 @@
+Write a function that returns the number of occurrences of an element in an array.
+
+Examples
+sample = [0, 1, 2, 2, 3]
+number_of_occurrences(0, sample) == 1
+number_of_occurrences(4, sample) == 0
+number_of_occurrences(2, sample) == 2
+number_of_occurrences(3, sample) == 1
+
+def number_of_occurrences(element, sample):
+    occurrences = 0
+    for i in sample:
+        if i == element:
+            occurrences += 1
+    return occurrences
